6f4c5944bb7189e5fdff37b8bc50f160e909a00c
ZaheerShaik1 <shaik.zaheer@veltris.com>
2024-09-26 22:57:16 +0530

    changes on assignment

--- assign.js
+++ assign.js
@@ -1,23 +1,11 @@
-// <button onclick="getResult()"> Get Results</button>
-//defining a function for finding a student grade
+
 function getalert(){
 	
 
 	const uname = document.getElementById('uname').value;
 	const pass = document.getElementById('pwd').value;
 
-	if(uname==null ){
-		alert("User name should not be empty");
-	}
-	else if(pass==null || pass.length < 6){
-		alert("Password required");
-	}
-	else{
-		alert("Login sucessful");
-		alert(`username : ${uname}`);
+	alert(uname.length<5?"User name should be minimum of length 5":(pass.length<9)?"password length should greater than 9":`Login sucessful ,Welcome : ${uname}`);
 
-	}
-	
-		// pw=+pw.value;
 	
 }
